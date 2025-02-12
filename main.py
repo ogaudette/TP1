5487fe9a0c2f54ac97a7991cc7dc3dc34bcaec88
Olivier Gaudette <6331477@cstjean.qc.ca>
2025-02-12 11:23:37 -0500

    finale

--- main.py
+++ main.py
@@ -36,19 +36,70 @@ print(amplitude_thermique(meteo_semaine[5]))
 
 #2b
 """ Créez un dictionnaire associant chaque jour à son amplitude thermique """
-jours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
+jours_semaine = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
 
-# FIX ME
-amplitude_jours = {jours: amplitude_thermique(meteo_semaine) for jours, meteo_semaine
-                   in enumerate(meteo_semaine)}
+amplitude_jours = {jours_semaine[i]: jour[1] - jour[0] for i, jour in enumerate(meteo_semaine)}
+
 print(amplitude_jours)
 
 # 2c
 """ Utilisez la notion de compréhension pour obtenir la liste des jours de pluie (pluie > 0) """
-# FIX ME
-pluie = {jours_pluie: True if meteo_semaine[2] > 0 else False for jours_pluie, meteo in meteo_semaine.items()}
-print(pluie)
+jours_de_pluie = {jours for jours in meteo_semaine if jours[2] > 0}
+print(jours_de_pluie)
 
 # 2d
 """ Utilisez filter pour obtenir les jours où la température max dépasse 25°C """
+jours_chauds = set(filter(lambda jour: jour[1] > 25, meteo_semaine))
+jours_semaine_chauds = [jours_semaine[i] for i, jour in enumerate(meteo_semaine) if jour in jours_chauds]
 
+print(jours_semaine_chauds)
+
+# 3
+""" Une matrice creuse est représentée par un dictionnaire sous la forme {(i,j): valeur} Supposons la 
+matrice creuse suivante: matrice = {(0,0): -1, (0,2): 3, (1,1): -2, (2,0): 4, (2,2): 5}"""
+matrice = {(0,0): -1, (0,2): 3, (1,1): -2, (2,0): 4, (2,2): 5}
+
+# 3a
+""" Utilisez une compréhension de dictionnaire pour créer un nouveau dictionnaire contenant 
+uniquement les éléments de la matrice d'origine ayant des valeurs strictement négatives.
+"""
+matrice_negative = {key: value for key, value in matrice.items() if value < 0}
+
+print(matrice_negative)
+
+# 3b
+""" Créez un ensemble (set) contenant les numéros de ligne (i) pour lesquelles il existe au moins une 
+valeur non nulle dans la matrice. """
+matrice_non_nulle = set(i for i, x in matrice.keys())
+
+print(matrice_non_nulle)
+
+# 3c
+"""Utilisez la fonction filter pour obtenir une liste des positions (i, j) dans la matrice où la valeur est 
+un nombre impair."""
+position_impair = set(filter(lambda i: matrice[i] % 2 != 0, matrice.keys()))
+print(position_impair)
+
+# 4
+""" Les données des étudiants ont été collectées et regroupées sous la forme suivante: (nom, age, 
+(note_math, note_info, note_physique)). Supposons la liste de tuples des étudiants d'une classe 
+etudiants = [ ('Alice', 18, (17, 15, 16)), ('Bob', 19, (12, 14, 11)), ('Charlie', 18, (15, 18, 14)), ('David', 20, 
+(9, 11, 10))] """
+etudiants = [ ('Alice', 18, (17, 15, 16)), ('Bob', 19, (12, 14, 11)), ('Charlie', 18, (15, 18, 14)), ('David', 20,
+(9, 11, 10))]
+
+# 4a
+""" Créez une fonction lambda qui calcule la moyenne des notes d'un étudiant """
+moyenne_notes = lambda etudiant: sum(etudiant[2]) / len(etudiant[2])
+print(moyenne_notes(etudiants[2]))
+
+# 4b
+""" Utilisez la notion de compréhension pour créer un dictionnaire {nom: moyenne} """
+dict_moyenne = {etudiant[0]: sum(etudiant[2]) / len(etudiant[2]) for etudiant in etudiants}
+print(dict_moyenne)
+
+# 4c
+""" Utilisez filter pour obtenir les étudiants ayant la moyenne en math """
+etudiants_pass_math = filter(lambda etudiant: etudiant[2][0] >= 10, etudiants)
+nom_pass_math = set(map(lambda etudiant: etudiant[0], etudiants_pass_math))
+print(nom_pass_math)
